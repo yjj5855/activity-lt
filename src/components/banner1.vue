76b5ffa38c71b276d98107c331ae7cc2d579df5b
yjj <yangjj@yowits.com>
2018-09-03 21:06:57 +0800

    no message

---
+++ src/components/banner1.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <x-img class="ximg-demo" :src="`static/b1-1.jpg`"/>
+    <x-img class="ximg-demo" :src="`static/b1-2.jpg`" :offset="-100"/>
+    <x-img class="ximg-demo" :src="`static/b1-3.jpg`" :offset="-100"/>
+    <x-img class="ximg-demo" :src="`static/b1-4.jpg`" :offset="-100"/>
+    <x-img class="ximg-demo" :src="`static/b1-5.jpg`" :offset="-100"/>
+  </div>
+</template>
+
+
+<script>
+  import {XImg} from 'vux'
+  export default {
+    components: {
+      XImg
+    },
+    data () {
+      return {
+        publicPath: 'https://yjj5855.github.io/activity-lt/dist/',
+        list: [
+        ]
+      }
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style>
+  .ximg-demo {
+    width: 100%;
+    height: auto;
+    display: block;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import banner1 from '../components/banner1'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/banner1',
+      name: 'banner1',
+      component: banner1
     }
   ]
 })
